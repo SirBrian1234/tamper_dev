e7ec4b87ffae3ae791da5567760bf72994651d46
kostis <kostiskag@gmail.com>
2017-03-03 21:15:41 +0200

    gpio test messages

--- GPIOTriggertest.py
+++ GPIOTriggertest.py
@@ -5,7 +5,7 @@ GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-print('Waiting to engage lock')
+print('Waiting to engage lock in GPIO input 18')
 while True:
     input_state = GPIO.input(18)
    
@@ -18,7 +18,7 @@ while True:
 while True:
     input_state = GPIO.input(18)
     if input_state == True:
-        print('Button Released')
+        print('Lock released')
 	break
     else:
 	time.sleep(0.2)
